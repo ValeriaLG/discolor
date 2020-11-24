78b7bbb068671073b3f664a6ff86d0216743522f
Valeria <RaspberryFruitt@gmail.com>
2020-11-24 00:44:52 -0500

    Updates

---
+++ precache-manifest.2c97a64531b17f25e0c28b5c567c5dd7.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "32c10e54f152f24be477fb8484853bae",
+    "url": "/discolor/index.html"
+  },
+  {
+    "revision": "f79e7e9dad8a3046b058",
+    "url": "/discolor/static/css/main.6714a1e2.chunk.css"
+  },
+  {
+    "revision": "853903cbb12eafca1575",
+    "url": "/discolor/static/js/2.f545a664.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/discolor/static/js/2.f545a664.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f79e7e9dad8a3046b058",
+    "url": "/discolor/static/js/main.fc8c73a0.chunk.js"
+  },
+  {
+    "revision": "bac25d05bfcd50881dd7",
+    "url": "/discolor/static/js/runtime-main.65f60d01.js"
+  },
+  {
+    "revision": "b186c70308660df65c7aaa655d0e8f34",
+    "url": "/discolor/static/media/discord-color-icon_lighter_version.b186c703.png"
+  },
+  {
+    "revision": "6c6374bad0b0b6d204d8d6dc4a18d820",
+    "url": "/discolor/static/media/discord.6c6374ba.woff"
+  },
+  {
+    "revision": "45db083a3a482e253cbb4a43fa302b64",
+    "url": "/discolor/static/media/discord_pfp_yellow.45db083a.png"
+  },
+  {
+    "revision": "b3c5fde0df79acf443086472b3b58a69",
+    "url": "/discolor/static/media/magnifying-glass.b3c5fde0.png"
+  },
+  {
+    "revision": "0e6261c72c6c067029a42d4679019661",
+    "url": "/discolor/static/media/written.0e6261c7.woff"
+  }
+]);
